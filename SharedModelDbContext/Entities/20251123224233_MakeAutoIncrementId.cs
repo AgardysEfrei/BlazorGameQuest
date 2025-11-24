ec597a1ca52579c819dedf1c6eff5ce86e822573
Vicen <seixas.vicente78@gmail.com>
2025-11-24 23:47:44 +0100

    Ajout des data postgre

---
+++ SharedModelDbContext/Entities/20251123224233_MakeAutoIncrementId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharedModelDbContext.Entities
+{
+    /// <inheritdoc />
+    public partial class MakeAutoIncrementId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
