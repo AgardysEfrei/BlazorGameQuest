157f574dc9f597e95333ccd922993bdfca311aed
theo <theo.klein@efrei.net>
2025-12-11 19:04:22 +0100

    keycloak

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,7 +1,10 @@
 using BlazorApp.Components;
 using Microsoft.AspNetCore.DataProtection;
-using BlazorAppApi.Service;
-using SharedModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +22,73 @@ builder.Services.AddHttpClient("Api", client =>
     client.BaseAddress = new Uri("http://blazorappapi:8080/"); 
     
 });
+
+var handler = new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+};
+var httpClient = new HttpClient(handler);
+
+builder.Services.AddSingleton(httpClient);
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+    {
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+        var backendIdpUrl = "http://keycloak:8080/realms/myrealm";
+        var clientIdpUrl = "http://localhost:8082/realms/myrealm";
+
+        options.Configuration = new()
+        {
+            Issuer = backendIdpUrl,
+            AuthorizationEndpoint = $"{clientIdpUrl}/protocol/openid-connect/auth",
+            TokenEndpoint = $"{backendIdpUrl}/protocol/openid-connect/token",
+            JwksUri = $"{backendIdpUrl}/protocol/openid-connect/certs",
+            JsonWebKeySet = FetchJwks($"{backendIdpUrl}/protocol/openid-connect/certs"),
+            EndSessionEndpoint = $"{clientIdpUrl}/protocol/openid-connect/logout"
+        };
+
+        foreach (var key in options.Configuration.JsonWebKeySet.GetSigningKeys())
+        {
+            options.Configuration.SigningKeys.Add(key);
+        }
+
+        options.ClientId = "blazor-gamequest";
+        options.TokenValidationParameters.ValidIssuers = [clientIdpUrl, backendIdpUrl];
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType = "role";
+        options.RequireHttpsMetadata = false;
+        options.ResponseType = OpenIdConnectResponseType.Code;
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.SaveTokens = true;
+        options.MapInboundClaims = true;
+
+        // options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+    })
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+
+JsonWebKeySet FetchJwks(string url)
+{
+    var result = httpClient.GetAsync(url).Result;
+    if (!result.IsSuccessStatusCode || result.Content is null)
+    {
+        throw new Exception(
+            $"Getting token issuers (Keycloaks) JWKS from {url} failed. Status code {result.StatusCode}");
+    }
+
+    var jwks = result.Content.ReadAsStringAsync().Result;
+    return new JsonWebKeySet(jwks);
+}
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddHttpContextAccessor();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,6 +101,11 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapPost("/logout", async (HttpContext context) =>
+{
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    await context.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+});
 
 app.UseAntiforgery();
 
